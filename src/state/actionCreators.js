f49a91038705f643c3276dec9e6f089c1b32bed8
John Afolabi <johnafolabi.da@gmail.com>
2019-12-12 09:44:02 +0100

    Setup reducer and actions

--- src/state/actionCreators.js
+++ src/state/actionCreators.js
@@ -1,13 +1,9 @@
 import * as types from "./actionTypes";
 
-export function increment() {
-  return {
-    type: types.INCREMENT
-  };
+export function add_Feature(feature) {
+  return { type: types.ADD_FEATURE, payload: feature };
 }
 
-export function decrement() {
-  return {
-    type: types.DECREMENT
-  };
+export function remove_Feature(feature) {
+  return { type: types.REMOVE_FEATURE, payload: feature };
 }
--- src/state/reducers.js
+++ src/state/reducers.js
@@ -1,14 +1,47 @@
 import * as types from "./actionTypes";
 
-const initialCount = 0;
+const initialState = {
+  additionalPrice: 0,
+  car: {
+    price: 26395,
+    name: "2019 Ford Mustang",
+    image:
+      "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
+    features: []
+  },
+  store: [
+    { id: 1, name: "V-6 engine", price: 1500 },
+    { id: 2, name: "Racing detail package", price: 1500 },
+    { id: 3, name: "Premium sound system", price: 500 },
+    { id: 4, name: "Rear spoiler", price: 250 }
+  ]
+};
 
-export function countReducer(count = initialCount, action) {
+export function reducer(state = initialState, action) {
   switch (action.type) {
-    case types.INCREMENT:
-      return count + 1;
-    case types.DECREMENT:
-      return count - 1;
+    case types.ADD_FEATURE:
+      return {
+        ...state,
+        additionalPrice: state.additionalPrice + action.payload.price,
+        car: {
+          ...state.car,
+          features: state.car.features.concat(action.payload)
+        },
+        store: state.store.filter(item => item.id !== action.payload.id)
+      };
+    case types.REMOVE_FEATURE:
+      return {
+        ...state,
+        additionalPrice: state.additionalPrice - action.payload.price,
+        car: {
+          ...state.car,
+          features: state.car.features.filter(
+            item => item.id !== action.payload.id
+          )
+        },
+        store: state.store.concat(action.payload)
+      };
     default:
-      return count;
+      return state;
   }
 }
