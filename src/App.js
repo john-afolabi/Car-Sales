5bfdf8e2822b00e345a514091ced3f4b4063bda1
John Afolabi <johnafolabi.da@gmail.com>
2019-12-12 17:06:57 +0100

    Setup states with Redux

--- src/App.js
+++ src/App.js
@@ -4,62 +4,40 @@ import Header from "./components/Header";
 import AddedFeatures from "./components/AddedFeatures";
 import AdditionalFeatures from "./components/AdditionalFeatures";
 import Total from "./components/Total";
+import { connect } from "react-redux";
+import * as actionCreators from "./state/actionCreators";
 
-import { countReducer } from "../src/state/reducers";
-import { combineReducers, createStore } from "redux";
-import { Provider } from "react-redux";
-import { Counter } from "./components/Counter";
-
-const rootReducer = combineReducers({
-  count: countReducer
-});
-
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: "2019 Ford Mustang",
-      image:
-        "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: "V-6 engine", price: 1500 },
-      { id: 2, name: "Racing detail package", price: 1500 },
-      { id: 3, name: "Premium sound system", price: 500 },
-      { id: 4, name: "Rear spoiler", price: 250 }
-    ]
-  };
-
-  const removeFeature = item => {
+const App = ({ add_Feature, remove_Feature, car, additionalPrice, store }) => {
+  const removeFeature = feature => {
     // dispatch an action here to remove an item
+    remove_Feature(feature);
   };
 
-  const buyItem = item => {
+  const addFeature = feature => {
     // dipsatch an action here to add an item
+    add_Feature(feature);
   };
 
   return (
-    <Provider store={store}>
-      <div className="boxes">
-        <div className="box">
-      <Counter />
-          <Header car={state.car} />
-          <AddedFeatures car={state.car} />
-        </div>
-        <div className="box">
-          <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-          <Total car={state.car} additionalPrice={state.additionalPrice} />
-        </div>
+    <div className="boxes">
+      <div className="box">
+        <Header car={car} />
+        <AddedFeatures car={car} removeItem={removeFeature} />
       </div>
-    </Provider>
+      <div className="box">
+        <AdditionalFeatures store={store} addFeature={addFeature} />
+        <Total car={car} additionalPrice={additionalPrice} />
+      </div>
+    </div>
   );
 };
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    additionalPrice: state.features.additionalPrice,
+    car: state.features.car,
+    store: state.features.store
+  };
+};
+
+export default connect(mapStateToProps, actionCreators)(App);
